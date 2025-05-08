dd07fa4265f257cd6799e4c685589bbe4ff68e71
Thys Call <thysmcall@gmail.com>
2025-05-08 00:06:50 -0600

    Phase 5 Client added, UI progress

--- client/src/main/java/client/Client.java
+++ client/src/main/java/client/Client.java
@@ -1,9 +1,9 @@
 package client;
 
-import model.LoginRequest;
-import model.LogoutRequest;
-import model.RegisterRequest;
+import model.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Client {
@@ -65,17 +65,30 @@ public class Client {
         }
     }
 
-    private void runLogout() {
-        // logout by making auth token null
+
+
+    private List<GameData> gamesList = new ArrayList<>();
+
+    private void runListGames() {
         try {
-            var req = new LogoutRequest(authToken);
-            server.logout(req);
-            authToken = null;
-            System.out.println("Log out successful!");
+            var result = server.listGames(authToken);
+            gamesList = result.games();
+            System.out.println("Games:");
+
+            for (int i = 0; i < gamesList.size(); i ++) {
+                var game = gamesList.get(i);
+                System.out.printf("%d. %s | White: %s | Black: %s%n",
+                        i + 1,
+                        game.gameName(),
+                        game.whiteUsername() != null ? game.whiteUsername() : "None",
+                        game.blackUsername() != null ? game.blackUsername() : "None"
+                );
+            }
         } catch (Exception error) {
-            System.out.println("Logout total failure..." + error.getMessage());
+            System.out.println("Failed to list games: " + error.getMessage());
         }
     }
+
 
     // login menu, what user will see (UI)
     private void loginHelp() {
@@ -127,6 +140,55 @@ public class Client {
             System.out.println(authData.username() + "logged in!");
         } catch (Exception error) {
             System.out.println("Registration failed... ");
+        }
+    }
+
+    private void runLogout() {
+        // logout by making auth token null
+        try {
+            var req = new LogoutRequest(authToken);
+            server.logout(req);
+            authToken = null;
+            System.out.println("Log out successful!");
+        } catch (Exception error) {
+            System.out.println("Logout total failure... " + error.getMessage());
+        }
+    }
+
+    private void runCreateGame() {
+        System.out.println("New game name: ");
+        String gameName = scanner.nextLine().trim();
+
+        try {
+            var result = server.createGame(authToken, new CreateGameRequest(gameName));
+            System.out.println("New Game Created! ID: " + result.gameID());
+        } catch (Exception error) {
+            System.out.println("Create game total failure... " + error.getMessage());
+        }
+    }
+
+    private void runJoinGame(String userColor) {
+        if (gamesList.isEmpty()) {
+            System.out.println("Games list is empty. Create a game name before joining one.");
+            return;
+        }
+        System.out.print("Enter game number to join game: ");
+
+        try {
+            int index = Integer.parseInt(scanner.nextLine().trim()) - 1;
+
+            if (index < 0 || index >= gamesList.size()) {
+                System.out.println("Game number is invalid");
+                return;
+            }
+
+            int gameID = gamesList.get(index).gameID();
+            server.joinGame(authToken, new JoinGameRequest(userColor, gameID));
+            System.out.println("Game joined!");
+        } catch (NumberFormatException error) {
+            System.out.println("Enter a valid game number");
+        } catch (Exception error) {
+            System.out.println("Failed to join game... " + error.getMessage());
         }
     }
 
